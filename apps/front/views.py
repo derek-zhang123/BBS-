046a892b6bb5c110a7fc524cd11f186537fb3edb
zhang-derek <1184405959@qq.com>
2018-06-09 15:15:40 +0800

    14.1注册完成后跳到上一个页面

--- apps/front/views.py
+++ apps/front/views.py
@@ -1,11 +1,14 @@
 # front/views.py
 __author__ = 'derek'
 
-from flask import Blueprint,views,render_template,make_response
-from utils.captcha import Captcha
-from io import BytesIO
+from flask import Blueprint, views, render_template, make_response,request
+from .forms import SignupForm
+from utils import restful,safeutils
+from .models import FrontUser
+from exts import db
 
-bp = Blueprint("front",__name__)
+bp = Blueprint("front", __name__)
+
 
 @bp.route('/')
 def index():
@@ -14,6 +17,25 @@ def index():
 
 class SignupView(views.MethodView):
     def get(self):
-        return render_template('front/signup.html')
+        return_to = request.referrer
+        if return_to and return_to != request.url and safeutils.is_safe_url(return_to):
+            return render_template('front/signup.html', return_to=return_to)
+        else:
+            return render_template('front/signup.html')
 
-bp.add_url_rule('/signup/',view_func=SignupView.as_view('signup'))
+    def post(self):
+        form = SignupForm(request.form)
+        if form.validate():
+            telephone = form.telephone.data
+            username = form.username.data
+            password = form.password.data
+            user = FrontUser(telephone=telephone, username=username, password=password)
+            db.session.add(user)
+            db.session.commit()
+            return restful.success()
+        else:
+            print(form.get_error())
+            return restful.params_error(message=form.get_error())
+
+
+bp.add_url_rule('/signup/', view_func=SignupView.as_view('signup'))
